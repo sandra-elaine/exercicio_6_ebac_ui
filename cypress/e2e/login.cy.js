5d480adcc35aa20373a22abceaa363f68e0b8f0e
Sandra <sandraelainedeoliveira@gmail.com>
2022-08-31 20:43:58 -0300

    Meu segundo commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,17 @@
 /// <reference types="cypress" />
 
 context('Funcionalidade Login', () =>{
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    //afterEach(() => {
+        //cy.screenshot()
+    //});
+
     
     it('Deve fazer login com sucesso', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -14,7 +22,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -24,7 +31,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste#teste')
         cy.get('.woocommerce-form > .button').click()
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            // Pegar o primeiro item da lista
+            //.first()
+
+            // Pegar o último item da lista
+            //.last()
+
+            // Pega um item específico da lista(0,1,2,3,...)
+            //.eq(3)
+            .contains('Ajax Full-Zip Sweatshirt')
+            .click()
+    });
+    it.only('Deve adicionar um produto no carrinho', () => {
+        var quantidade = 2
+
+        cy.get('[class="product-block grid"]')
+            .contains('Ajax Full-Zip Sweatshirt').click()
+            cy.get('.button-variable-item-M').click()
+                cy.get('.button-variable-item-Green').click()
+                cy.get('.input-text').clear().type(quantidade)
+                cy.get('.single_add_to_cart_button').click()
+
+                cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+                
+    });
+
+});
