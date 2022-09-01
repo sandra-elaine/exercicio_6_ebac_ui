76f07dbc81979304cbca1adf5ca43e3cb06a15d4
Sandra <sandraelainedeoliveira@gmail.com>
2022-09-01 19:27:30 -0300

    Alteração em produtos

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -30,6 +30,7 @@ describe('Funcionalidade Página de Produtos', () => {
                 cy.get('.single_add_to_cart_button').click()
 
                 cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+                cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
                 
     });
 
